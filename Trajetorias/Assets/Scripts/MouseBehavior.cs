0ff87a3710f0a59190ebd83ae178bb6f281ec074
Raissa Scheeren <raissasche@gmail.com>
2021-08-30 14:49:00 -0300

    ratinho pululandinho

---
+++ Trajetorias/Assets/Scripts/MouseBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseBehavior : MonoBehaviour
+{
+
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        Vector3 dir = (target.transform.position - transform.position).normalized;
+        dir.y = 0;
+ 
+        Quaternion rot = Quaternion.LookRotation(dir);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, 10 * Time.deltaTime);
+
+    }
+}
