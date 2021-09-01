bfee6e5fb7a512bb0e41e920ad5ef7c932214e0f
Raissa Scheeren <raissasche@gmail.com>
2021-09-01 18:21:28 -0300

    implementando wall following

---
+++ Trajetorias/Assets/Scripts/WallFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallFollow : MonoBehaviour
+{
+ public float speed;
+ 
+     void Update()
+     {
+         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+         RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1/*, layerMask*/))
+        {
+             transform.Rotate(1, 90, 1);
+        }
+     }
+}
